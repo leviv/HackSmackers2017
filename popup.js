81198493a7189e885b1c9ef7df2edeb2565b2f20
Levi V <villarreallevi@gmail.com>
2017-10-29 11:31:58 -0500

    Update the page text dynamically
    update text properties when the extension is clicked

--- popup.js
+++ popup.js
@@ -1,7 +1,24 @@
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+font = localStorage["font-family-storage"];
+$('.textbox').css("font-family", font);
+$('select option[value="' + font + '"]').prop('selected', true);
+      
 
+fontColor = localStorage["font-color-storage"];
+var colorcolor = document.getElementById('font-color');  
+colorcolor.value = fontColor;
+colorcolor.setAttribute("style", "background-color: #" + fontColor);
+$('.textbox').css("color", fontColor);
+
+
+fontBg = localStorage["font-bg-storage"];
+var bgcolorcolor = document.getElementById('font-bg');  
+bgcolorcolor.value = fontBg;
+bgcolorcolor.setAttribute("style", "background-color: #" + fontBg);
+$('.textbox').css("background", fontBg);
+hello();
 /**
  * Get the current URL.
  *
@@ -47,128 +64,28 @@ function getCurrentTabUrl(callback) {
     // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
-/**
- * Change the background color of the current page.
- *
- * @param {string} color The new background color.
- */
-function changeFontColor(color) {
-    
-    var script = 'document.body.style.color="' + color + '";';
-    // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-    // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-    // into a page. Since we omit the optional first argument "tabId", the script
-    // is inserted into the active tab of the current window, which serves as the
-    // default.
-    chrome.tabs.executeScript({
-        code: script
-    });
-}
-
-function changeFont(font) {
-    var script = 'document.body.style.fontFamily="' + font + '";';
-    // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-    // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-    // into a page. Since we omit the optional first argument "tabId", the script
-    // is inserted into the active tab of the current window, which serves as the
-    // default.
-    chrome.tabs.executeScript({
-        code: script
-    });
-}
-
-function changeFontBgColor(bgColor) {
-    var script = 'document.body.style.backgroundColor="' + bgColor + '";';
-    // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-    // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-    // into a page. Since we omit the optional first argument "tabId", the script
-    // is inserted into the active tab of the current window, which serves as the
-    // default.
-    chrome.tabs.executeScript({
-        code: script
-    });
-}
-
-/**
- * Gets the saved background color for url.
- *
- * @param {string} url URL whose background color is to be retrieved.
- * @param {function(string)} callback called with the saved background color for
- *     the given url on success, or a falsy value if no color is retrieved.
- */
-function getSavedBackgroundColor(url, callback) {
-    // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-    // for chrome.runtime.lastError to ensure correctness even when the API call
-    // fails.
-    chrome.storage.sync.get(url, (items) => {
-        callback(chrome.runtime.lastError ? null : items[url]);
-    });
-}
-
-/**
- * Sets the given background color for url.
- *
- * @param {string} url URL for which background color is to be saved.
- * @param {string} color The background color to be saved.
- */
-function saveBackgroundColor(url, color) {
-    var items = {};
-    items[url] = color;
-    // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-    // optional callback since we don't need to perform any action once the
-    // background color is saved.
-    chrome.storage.sync.set(items);
-}
-
-// This extension loads the saved background color for the current tab if one
-// exists. The user can select a new background color from the dropdown for the
-// current page, and it will be saved as part of the extension's isolated
-// storage. The chrome.storage API is used for this purpose. This is different
-// from the window.localStorage API, which is synchronous and stores data bound
-// to a document's origin. Also, using chrome.storage.sync instead of
-// chrome.storage.local allows the extension data to be synced across multiple
-// user devices.
-document.addEventListener('DOMContentLoaded', () => {
-    getCurrentTabUrl((url) => {
-        var dropdown = document.getElementById('font-bg');
-
-        // Load the saved background color for this page and modify the dropdown
-        // value, if needed.
-        getSavedBackgroundColor(url, (savedColor) => {
-            if (savedColor) {
-                changeFontColor(savedColor);
-                dropdown.value = savedColor;
-            }
-        });
-
-        // Ensure the background color is changed and saved when the dropdown
-        // selection changes.
-        dropdown.addEventListener('change', () => {
-            changeFontColor(dropdown.value);
-            saveBackgroundColor(url, dropdown.value);
-        });
-    });
-});
-
-
-
 var font = "";
 var fontColor = "";
 var fontBg = "";
 
+    
+
 $("#font").change(function() {
     $('.textbox').css("font-family", $(this).val());
     font = $(this).val();
+    localStorage["font-family-storage"] = font;
 });
 
 $("#font-color").change(function() {
     $('.textbox').css("color", ("#" + $("#font-color").val()));
     fontColor = ("#" + $("#font-color").val());
+    localStorage["font-color-storage"] = fontColor;
 });
 
 $("#font-bg").change(function() {
     $('.textbox').css("background-color", ("#" + $("#font-bg").val()));
     fontBg = ("#" + $("#font-bg").val());
+    localStorage["font-bg-storage"] = fontBg;
 });
 
 function hello() {
