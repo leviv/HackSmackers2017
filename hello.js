c07b8d35a473127681bc09fe283414f2126b9d44
Levi V <villarreallevi@gmail.com>
2017-10-29 05:46:43 -0500

    Change text properties statically
    getting close

---
+++ hello.js
@@ -0,0 +1,5 @@
+var yeet = document.querySelectorAll('p,li,h1,h2,h3,h4,h5,h6');
+
+for (i = 0; i < yeet.length; i++) {
+    yeet[i].setAttribute("style","color: purple; background-color: #000; font-family: monospace");
+}
